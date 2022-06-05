d4d270203d6b274679f43d4b26a1bdd0cf67f17f
Roman Ravas <romorko@gmail.com>
2022-06-05 11:17:03 +0200

    Konstruktory pridane

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,26 @@
-class noNumber(message: String) : Exception(message)
 
-class Bod(private var x: Int = 0, private var y: Int = 0)
+fun getInt(text: String = "Zadaj cislo:", min: Int = -10, max: Int = 10): Int {
+    var cislo: Int
+    while (true) {
+        try {
+            print("$text z intervalu <$min , $max>: ")
+            cislo = readLine()!!.toInt()
+            if (cislo < min || cislo > max) {
+                throw NoNumber("Zadane cislo $cislo nepatri do stanoveneho intervalu <$min,$max>!\n")
+            }
+            return cislo
+        }
+        catch (e: NumberFormatException) {
+            print("Nebolo zadane cislo!")
+        }
+        catch (e: NoNumber) {
+            print(e.message)
+        }
+    }
+}
+class NoNumber(message: String) : Exception(message)
+
+class Bod(private var x: Int = 0, private var y: Int = 1)
 {
     constructor() : this(0, 0)
     {
@@ -10,8 +30,8 @@ class Bod(private var x: Int = 0, private var y: Int = 0)
             val cisla = readLine()?.split(Regex(","))
             try
             {
-                x = cisla?.get(0)?.toInt() ?: 0
-                y = cisla?.get(1)?.toInt() ?: 0
+                x = cisla?.get(0)?.trim()?.toInt() ?: 0
+                y = cisla?.get(1)?.trim()?.toInt() ?: 0
                 break
             }
             catch (e: NumberFormatException)
@@ -19,34 +39,19 @@ class Bod(private var x: Int = 0, private var y: Int = 0)
                 print("Bolo najdeny nepovoleny znak! " + e.message+". ")
             }
         }
-
     }
 
     override fun toString(): String = "[$x,$y]"
 
-    fun getInt(text: String = "Zadaj cislo:", min: Int = -10, max: Int = 10): Int {
-        var cislo: Int
-        while (true) {
-            try {
-                print("$text z intervalu <$min , $max>: ")
-                cislo = readLine()!!.toInt()
-                if (cislo < min || cislo > max) {
-                    throw noNumber("Zadane cislo $cislo nepatri do stanoveneho intervalu <$min,$max>!\n")
-                }
-                return cislo
-            }
-            catch (e: NumberFormatException) {
-                print("Nebolo zadane cislo!")
-            }
-            catch (e: noNumber) {
-                print(e.message)
-            }
-        }
-    }
+
 }
 
 fun main() {
-    val A = Bod()
+    val A = Bod(3)
     println(A)
+    val B = Bod(3,5)
+    print(B)
+    val C= Bod()
+    print(C)
 //println("Program arguments: ${args.joinToString()}")
 }
