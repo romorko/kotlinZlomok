6f0ca246e6e6179bb1d2d797bd728e36c6fb9e23
Roman Ravas <romorko@gmail.com>
2022-06-07 17:37:56 +0200

    pretazene operatory zlomok

--- src/main/kotlin/Zlomok.kt
+++ src/main/kotlin/Zlomok.kt
@@ -1,5 +1,5 @@
 fun getInt(
-    text: String = "Zadaj cislo:",
+    text: String = "Zadaj cislo ",
     min: Int = -100,
     max: Int = 100,
     ajNula: Boolean = true,
@@ -10,7 +10,7 @@ fun getInt(
     {
         try
         {
-            print("$text z intervalu <$min , $max>: ")
+            print("$text z intervalu:<$min , $max>, nula: " + if (ajNula) "povolena: " else "zakazana: ")
             cislo = readLine()!!.toInt()
             if (cislo < min || cislo > max)
             {
@@ -69,6 +69,10 @@ class Zlomok(private var cit: Int, private var men: Int = 1)
             print("Menovatel nesmie byt nula! ")
             men = getInt(ajNula = false)
         }
+        val spolocny=nsd(cit,men)
+        cit/=spolocny
+        men/=spolocny
+
     }
 
     constructor(zlom: String) : this(0, 1)
@@ -78,6 +82,9 @@ class Zlomok(private var cit: Int, private var men: Int = 1)
             val cisla = zlom.split(Regex("/"))
             cit = checkInt(ajNulaMozna = true, preved = cisla[0].trim())
             men = checkInt(ajNulaMozna = false, preved = cisla[1].trim())
+            val spolocny=nsd(cit,men)
+            cit/=spolocny
+            men/=spolocny
         }
         else
         {
@@ -89,5 +96,23 @@ class Zlomok(private var cit: Int, private var men: Int = 1)
     override fun toString(): String
     {
         return "$cit/$men"
+    }
+    operator fun Zlomok.plus(other:Zlomok)=Zlomok(cit*other.men+men*other.cit,men*other.men)
+    operator fun Zlomok.minus(other:Zlomok)=Zlomok(cit*other.men-men*other.cit,men*other.men)
+    operator fun Zlomok.times(other:Zlomok)=Zlomok(cit*other.cit,men*other.men)
+    operator fun Zlomok.div(other:Zlomok)=Zlomok(cit*other.men,men*other.cit)
+
+    fun nsd(nom:Int,den:Int):Int
+    {
+        var t:Int
+        var d = den
+        var n = nom
+        while (d != 0)
+        {
+            t = d;
+            d = nom % den;
+            n = t;
+        }
+        return n
     }
 }
